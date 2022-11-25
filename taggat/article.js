29da163cc68f7cc94f0c4bc072c824fd4448037b
SilkySquidward <masterskysword@gmail.com>
2022-11-25 19:09:05 +0000

    dor de cabeça nos articles

--- taggat/article.js
+++ taggat/article.js
@@ -1,6 +1,7 @@
 // use DOM load event listener to wait for content on page
 addEventListener("load", init);
 
+
 function init() {
   console.log("ready when you are!");
   // ehn page ready get the JSON content (timeline posts)
@@ -8,39 +9,30 @@ function init() {
 }
 
 
+
 function getSome() {
-    //console.log("getting json");
-  
-    // JSON url endpoint (wordpress timeline category = 13)
-    let url = "https://nit.fba.up.pt/dev/wp-json/wp/v2/posts?categories=13";
-  
+  //console.log("getting json");
 
-    // see the files from class 6 for fetch operations
+  // JSON url endpoint (wordpress timeline category = 13)
+  let url = "https://nit.fba.up.pt/dev/wp-json/wp/v2/posts";
+
+  // see the files from class 6 for fetch operations
   fetch(url)
-  .then(function (resposta) {
-    return resposta.json();
-  })
-  .then(function (dados) {
-    //console.log(dados);
+    .then(function (resposta) {
+      return resposta.json();
+    })
+    .then(function (dados) {
+      //console.log(dados);
 
-    // dados = array of 9 or more posts
-    // for each post call a new function with the post content
-    for (let post of dados) {
-      buildPost(post);
-    }
-
-    // run a second loop to change the posts' category names
-    for (let post of dados) {
-        // after the element span is on the page, run a second fetch (<-- because it may take some time to run)
-        // and replace the contents of the spans with the specific classes with their corresponding names (or just use an if…)
-        fetchCategory(post.categories[0]);
-      }
-
-      // and run a third loop to change the posts' images
-      // this one we didn't do in class. same as category, but with an image url fetch
+      // dados = array of 9 or more posts
+      // for each post call a new function with the post content
       for (let post of dados) {
-        fetchFeaturedMedia(post.id, post.featured_media);
+        buildPost(post);
       }
+      // run a second loop to change the posts' category names
+     
+      // and run a third loop to change the posts' images
+   
     })
     .catch(function (error) {
       console.log(error);
@@ -48,38 +40,69 @@ function getSome() {
 }
 
 
+
+// this function is responsible for
+// 1. creating a new element for each post in the JSON
+// 2. getting the relevant content of the post
+// 3. note that some content is NOT inside the JSON --> you have to use a second, or third fetch to get them
+// Main (top-level) Category: Timeline = 13
+// Timeline sub-categories: Artefactos = 37; Locais = 52; Pessoas = 14
+// Other "second-level-fetch" data is the feature image. (see below)
+
+// when calling the function we pass the specific "_post" as an argument
+// the function receives the "_post" and…
+
 function buildPost(_post) {
-    // create a new element
-    let el = document.createElement("article");
-    let myID = "id-" + _post.id;
+  // create a new element
+  let el = document.createElement("article");
+  let myID = "id-" + _post.id;
+
+
+
+
   
-    el.setAttribute("id", myID);
 
+  el.setAttribute("id", myID);
 
- // use string/template literals to build the HTML object
-  el.innerHTML = `<figure >
-                        <img src="">
-                        <figcaption>Caption of image</figcaption>
-                        </figure>
+  // use string/template literals to build the HTML object the title
+  el.innerHTML = `
 
                         <!-- inspect the JSON object in the browser to find out the necessary data -->
                         <h1>${_post.title.rendered}</h1>
 
-                        <div class="meta">
-                            <span class="data">${_post.acf.data}</span>
-                            <span class="local">${_post.acf.local}</span>
-
-                            <!-- this one is trickier… the origina JSON post only has the ID (number) of the category-->
-                            <!-- we can place the number as a class "eg ".c14" in the element -->
-                            <!-- and later we run a second fetch loop to set the innerHTML of these elements with their name -->
-                            <span class="categoria c${_post.categories[0]}">duh… what num?</span>
-                        </div>
-
-                        <p>${_post.content.rendered}</p>`;
+                    </div>
 
 
-                    
-                        // place the new element on the page
-  document.querySelector("#posts").appendChild(el);
+                       `;
+
+  // place the new element on the page
+  document.querySelector("#titulo").appendChild(el);
   console.log("built article", el);
+}
+
+
+
+function buildDate(_post) {
+  // create a new element
+  let el = document.createElement("article");
+  let myID2 = "id-" + _post.date;
+
+
+
+
+  
+
+  el2.setAttribute("id", myID2);
+
+  // use string/template literals to build the HTML object the title
+  el2.innerHTML = `
+
+                        <!-- inspect the JSON object in the browser to find out the necessary data -->
+                        <h1>${_post.acf.data}</h1>
+
+                       `;
+
+  // place the new element on the page
+  document.querySelector("#data").appendChild(el);
+  console.log("built date", el2);
 }
