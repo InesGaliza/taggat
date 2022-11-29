fe88cc0f77497327642821d9fac989cbe521f1f2
ctmagalhaes <carolina.magalhaesbruh@gmail.com>
2022-11-29 22:09:51 +0000

    INICIO CAT (LANDING PAGE)

--- taggat/main.js
+++ taggat/main.js
@@ -8,56 +8,50 @@ function inicio() {
   // FUNÇÃO PARA FAZER O FETCH
   vaiBuscar();
 
-  // BOTÃO > TOGGLE CLASS / HOVER / AMINAÇÃO
+  // MENU FILTROS >CATEGORIAS > CRIAR ESTE MENU
+  criarCategoras();
+
+  // BOTÃO > ADD&REMOVE CLASS / ENTER&LEAVE / AMINAÇÃO
   botaoHover();
+  botaoClick();
 }
 
 /*------------------------------------------------------------------------------------------------------------------------------*/
-
+/*------------------------------------------------------------------------------------------------------------------------------*/
 function vaiBuscar() {
     // DECLARAR O URL DO WORDPRESS
     let urlBase = "https://nit.fba.up.pt/dev/wp-json/wp/v2/posts?categories=13";
-
     // 1ª RONDA DE FETCH
     fetch(urlBase)
     .then(function (resposta) {
       return resposta.json();
     })
-
     .then(function (dados) {
     console.log(dados);
-    
     // PARA CADA ENTRADA DO WORDPRESS (CADA OBJETO DO ARRAY) CONTROI UM ARTIGO (FUNÇÃO)
     for (let post of dados) {
         construirArtigo(post);
     }
-    
     // DEPOIS DA CONSTRUÇÃO DO ARTIGO, PARA CADA ENTRADA DO WORDPRESS, CORRE UM SEGUNDO FETCH (FUNÇÃO)
     for (let post of dados) {
         fetchCategoria(post.categories[0]);
     }
-
     // DEPOIS, CORRER UM TERCEIRO FETCH
     for (let post of dados) {
       fetchImagens(post.id, post.featured_media);
     }
     })
-
     .catch(function (error) {
         console.log(error);
     })
 }
-
 /*------------------------------------------------------------------------------------------------------------------------------*/
-
 // FUNÇÃO PARA CONSTRUIR OS ARTIGOS
 function construirArtigo(_post) {
     // CRIAR UM NOVO ELEMENTO > O ARTIGO
     let el = document.createElement("article");
     let myID = "id-" + _post.id;
-  
     el.setAttribute("id", myID);
-  
     // UTLIZAR STRING / TEMPLATE LITERALS PARA CONSTRUIR O OBJETO HTML
     el.innerHTML = `<figure >
                           <img src="">
@@ -82,16 +76,13 @@ function construirArtigo(_post) {
     document.querySelector("#NovoPost").appendChild(el);
     //console.log("o Artigo está constrído!", el);
   }
-
 /*------------------------------------------------------------------------------------------------------------------------------*/
-
 // FUNÇÃO QUE CORRE O SEGUNDO FETCH
 function fetchCategoria(_cat_num) {
     //console.log("fetching categories names!");
-  
+
     // ESTE É O URL PARA IR BUSCAR OS NOMES DAS CATEGORIAS
     let url = "https://nit.fba.up.pt/dev/wp-json/wp/v2/categories/";
-  
 
     // O URL FINAL DEVE SER ALGO DO GÉNERO...
     // https://nit.fba.up.pt/dev/wp-json/wp/v2/categories/14
@@ -106,12 +97,9 @@ function fetchCategoria(_cat_num) {
         // DEFINIR UM NOVO TEXT STRING PARA CONSTRUIR O NOME DA CALSSE
         let myClass = ".c" + _cat_num;
         //console.log("my class", myClass);
-  
         // NO DOM, SELECIONAR TODOS OS ELEMENTOS COM A CLASSE (<-- querySelectorAll > ARRAY)
         let els = document.querySelectorAll(myClass);
-  
-        // EM CADA ELEMENTO
-        // MUDAR O INNERHTML PARA O NOME DA CATEGORIA DESSE NÚMERO (WORDPRESS)
+        // EM CADA ELEMENTO, MUDAR O INNERHTML PARA O NOME DA CATEGORIA DESSE NÚMERO (WORDPRESS)
         for (let el of els) {
           el.innerHTML = dados.name;
         }
@@ -120,21 +108,17 @@ function fetchCategoria(_cat_num) {
         console.log(error);
       });
   }
- 
 /*------------------------------------------------------------------------------------------------------------------------------*/
-
 // FUNÇÃO QUE CORRE O TERCEIRO FETCH
 async function fetchImagens(_id, _media) {
   console.log("estou fetchar o media!");
   //console.log(_id);
   //console.log(_media);
-
   // ESTE É O URL PARA IR BUSCAR O MEDIA (IMAGENS)
   let mediaURL = "https://nit.fba.up.pt/dev/wp-json/wp/v2/media/";
   // ADICIONAR O NÚMERO DO MEDIA AO URL
   mediaURL += _media;
   //console.log(mediaURL);
-
   let mySrc = "";
 
   const resposta = await fetch(mediaURL);
@@ -144,10 +128,8 @@ async function fetchImagens(_id, _media) {
   }
 
   const dados = await resposta.json();
-
   mySrc = dados.media_details.sizes.thumbnail.source_url;
   //console.log("featured media scr url", mySrc);
-
   let myID = "#id-" + _id;
   let myEl = document.querySelector(myID);
   myEl.children.item(0).children.item(0).setAttribute("src", mySrc);
@@ -155,12 +137,67 @@ async function fetchImagens(_id, _media) {
 
 /*------------------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------------------*/
+// FUNÇÃO PARA CRIAR FILTROS > CATEGORIAS
+function criarCategoras() {
+  let ul = document.createElement('ul');
+  ul.innerHTML = `<li><button id="btn1" class="filtros"><span class="bi bi-arrow-left-circle"></span>ARTEFACTOS</button></li>
+                  <li><button id="btn2" class="filtros"><span class="bi bi-arrow-left-circle"></span>LOCAIS</button></li>
+                  <li><button id="btn3" class="filtros"><span class="bi bi-arrow-left-circle"></span>PESSOAS</button></li>`
+  document.querySelector("aside").appendChild(ul);
 
-// BOTÃO > TOGGLE CLASS / HOVER / AMINAÇÃO
+}
+/*------------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------------------------------*/
+// FUNÇÃO PARA BOTÃO > ADD&REMOVE CLASS / ENTER&LEAVE / AMINAÇÃO
 function botaoHover() {
-$("button").hover(function() {
-  console.log("hovering!");
-  $("button, #arrowDown").toggleClass("btnHover");
-  $("#arrowDown").toggleClass("btnHover:after");
+// BOTÃO "VER A TIMELINE" > MOUSEENTER/MOUSELEAVE
+$("button#timeline").mouseenter(function() {
+  //console.log("i'm in!");
+  $(this).addClass("btnHover");
+  $("#boot-icon, #boot-icon2").addClass("seta");
+  $("#boot-icon, #boot-icon2").css("background-color", "var(--black)");
 })
+$("button#timeline").mouseleave(function() {
+  //console.log("i'm out!");
+  $(this).removeClass("btnHover");
+  $("#boot-icon, #boot-icon2").removeClass("seta");
+  $("#boot-icon, #boot-icon2").css("background-color", "var(--antwhite)");
+})
+
+// BOTÃO FILTROS > MOUSEENTER/MOUSELEAVE
+$("button.filtros").each(function() {
+  $(this).mouseenter(function() {
+    //console.log("i'm in!");
+    $(this).addClass("btnHover");
+
+    $(this).children(".bi-arrow-left-circle").each(function () {
+      $(this).addClass("seta");
+      $(this).css("background-color", "var(--black)");
+    })
+
+  })
+  $(this).mouseleave(function() {
+    //console.log("i'm out!");
+    $(this).removeClass("btnHover");
+      $(".bi-arrow-left-circle").removeClass("seta");
+      $(".bi-arrow-left-circle").css("background-color", "var(--antwhite)");
+  })
+})
+}
+/*------------------------------------------------------------------------------------------------------------------------------*/
+// BOTÃO CLICK > PARA FILTRAR CATEGORIAS
+function botaoClick() {
+  $("button.filtros").each(function() {
+      $(this).click(function() {
+        console.log("i've been clicked!");
+        if(this.id === 'btn1') {
+          console.log("i'm button 1!");
+        } else if(this.id === 'btn2') {
+          console.log("i'm button 2!");
+        } else if(this.id === 'btn3') {
+          console.log("i'm button 3!")
+        }
+
+      })
+  })
 }
