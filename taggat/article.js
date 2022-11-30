e39c847e2116115497b4fb31667e5d8cfadea9c9
SilkySquidward <masterskysword@gmail.com>
2022-11-30 16:10:17 +0000

    article page: fetches a funcionarem! post e titulo

--- taggat/article.js
+++ taggat/article.js
@@ -13,6 +13,7 @@ const urlParams = new URLSearchParams(queryString);
 
 // the urlParams returns an object. Get a specific "property" (AKA variable) of the object (URL)
 // eg. the "403" in the "http://127.0.0.1:5500/single.html?post=403" URL
+const title = urlParams.get("title");
 const post = urlParams.get("post");
 //console.log(post);
 
@@ -35,25 +36,37 @@ fetch(url)
   })
   .then(function (dados) {
     // não esquecer que devolve um array (só com 1 post)
+    for (let title of dados) {
+      buildTitle(title);
+    }
     for (let post of dados) {
       buildPost(post);
     }
+    for (let data of dados) {
+      buildData(data);
+    }
   })
+
+
+
   .catch(function (error) {
     console.log(error);
   });
 
-function buildPost(_post) {
+
+  // funcao titulo 
+
+function buildTitle(_title) {
   // create a new element
   let el = document.createElement("article");
-  let myID = "id-" + _post.id;
+  let myID = "id-" + _title.id;
 
   el.setAttribute("id", myID);
 
   // use string/template literals to build the HTML object
-  el.innerHTML = `<h1>${_post.title.rendered}</h1>
+  el.innerHTML = `<h1>${_title.title.rendered}</h1>
 
-  <div> </div>
+
   
                         
   
@@ -64,3 +77,60 @@ function buildPost(_post) {
   
   console.log("built article", el);
 }
+
+
+
+
+
+
+  // funcao post 
+
+  function buildPost(_post) {
+    // create a new element
+    let elpost = document.createElement("article");
+    let myID = "id-" + _post.id;
+  
+    elpost.setAttribute("id", myID);
+  
+    // use string/template literals to build the HTML object
+    elpost.innerHTML = `<p>${_post.content.rendered}</p>
+  
+  
+    
+                          
+    
+                          `;
+  
+    // place the new element on the page
+    document.querySelector("#post").appendChild(elpost);
+    
+    console.log("built post", elpost);
+  }
+
+
+
+
+    // funcao data 
+
+    function buildData(_data) {
+      // create a new element
+      let eldata = document.createElement("article");
+      let myID = "id-" + _data.id;
+    
+      eldata.setAttribute("id", myID);
+    
+      // use string/template literals to build the HTML object
+      eldata.innerHTML = `<p>${_post.acf.data}</p>
+    
+    
+      
+                            
+      
+                            `;
+    
+      // place the new element on the page
+      document.querySelector("#data").appendChild(eldata);
+      
+      console.log("built data", eldata);
+    }
+  
