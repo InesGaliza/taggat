53a716e4db6f4d28e6388948ef36a641b73d0c23
SilkySquidward <masterskysword@gmail.com>
2022-11-30 14:33:37 +0000

    Ajeitei o fetch no article

--- taggat/article.js
+++ taggat/article.js
@@ -1,98 +1,66 @@
-// use DOM load event listener to wait for content on page
-addEventListener("load", init);
+console.log("hello single page mode");
 
+// reading parameters from the search / URL bar
+// https://www.sitepoint.com/get-url-parameters-with-javascript/
 
-function init() {
-  console.log("ready when you are!");
-  
-  getSome();
-}
+// this gets the URL address bar
+const queryString = window.location.search;
 
+// this breaks down the URL and gets the stuff after the HTML page
+// eg. the string "v=0AsAjHPupKQ" after "https://www.youtube.com/watch?" in a youtube address
+const urlParams = new URLSearchParams(queryString);
+//console.log(urlParams);
 
+// the urlParams returns an object. Get a specific "property" (AKA variable) of the object (URL)
+// eg. the "403" in the "http://127.0.0.1:5500/single.html?post=403" URL
+const post = urlParams.get("post");
+//console.log(post);
 
-function getSome() {
-  //console.log("getting json");
+// fazer o fetch do post específico passado no URL n.º 403 do JSON do Wordpress
+// só um post/objeto JSON:
+// https://nit.fba.up.pt/dev/wp-json/wp/v2/posts/403
+// ligeiramente diferente do nosso método
 
-  // JSON url endpoint (wordpress timeline category = 13)
-  let url = "https://nit.fba.up.pt/dev/wp-json/wp/v2/posts?include=403";
+// Método identico ao anterior Fetch de um array com apenas um post ou com posts específicos
+// https://nit.fba.up.pt/dev/wp-json/wp/v2/posts?include=403 (array só com um)
+// https://nit.fba.up.pt/dev/wp-json/wp/v2/posts?include=403,397
 
+// Fetch de um post (construir a edenreço do JSON com o parâmetro da URL )
+let url = "https://nit.fba.up.pt/dev/wp-json/wp/v2/posts?include=403";
 
-  fetch(url)
-    .then(function (resposta) {
-      return resposta.json();
-    })
-    .then(function (titulodados) {
-      //console.log(dados);
+// see the files from class 6 for fetch operations
+fetch(url)
+  .then(function (resposta) {
+    return resposta.json();
+  })
+  .then(function (dados) {
+    // não esquecer que devolve um array (só com 1 post)
+    for (let post of dados) {
+      buildPost(post);
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-      // for each post call a new function with the post content
-      for (let post of titulodados) {
-        buildTitle(post);
-
-      }
-
-      //data
-
-      for (let post of titulodados) {
-        buildTitle(post);
-
-      }
-    
-      // and run a third loop to change the posts' images
-
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-
-
-// this function is responsible for
-// 1. creating a new element for each post in the JSON
-// 2. getting the relevant content of the post
-// 3. note that some content is NOT inside the JSON --> you have to use a second, or third fetch to get them
-// Main (top-level) Category: Timeline = 13
-// Timeline sub-categories: Artefactos = 37; Locais = 52; Pessoas = 14
-// Other "second-level-fetch" data is the feature image. (see below)
-
-// when calling the function we pass the specific "_post" as an argument
-// the function receives the "_post" and…
-
-function buildTitle(_post) {
+function buildPost(_post) {
   // create a new element
   let el = document.createElement("article");
   let myID = "id-" + _post.id;
 
-
-
-
-
   el.setAttribute("id", myID);
 
-  // titulo insert
-  el.innerHTML = `
-               
+  // use string/template literals to build the HTML object
+  el.innerHTML = `<h1>${_post.title.rendered}</h1>
+
+  <div> </div>
   
-
-                        <!-- inspect the JSON object in the browser to find out the necessary data -->
-                        <h1>${_post.title.rendered}</h1>
-
-                
-
-
-                       `;
-
-
+                        
+  
+                        `;
 
   // place the new element on the page
   document.querySelector("#titulo").appendChild(el);
+  
   console.log("built article", el);
-
 }
-
-
-
-
-
-
-
