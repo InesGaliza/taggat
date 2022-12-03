21812cce4ae812e02aeeb0edff1da643d3f8d2ea
Sara Pereira <114031895+Saruvsky@users.noreply.github.com>
2022-12-03 19:33:10 +0000

    Alteração de classes e melhoria do about

--- taggat/about.js
+++ taggat/about.js
@@ -2,243 +2,50 @@ addEventListener("load", inicio);
 
 
 function inicio() {
-  /*console.log("está pronto!");
 
-    let TAGGATMostraCaixa = document.querySelector('#TAGGATMostraCaixa');
-    let TAGLINEMostraCaixa = document.querySelector('#TAGLINEMostraCaixa');
-    let TAGGATCaixa = document.querySelector('#TAGGATCaixa');
-    let TAGLINECaixa = document.querySelector('#TAGLINECaixa');
-    let CaixaTAGGATTAGLINE = document.querySelector("#CaixaTAGGATTAGLINE");
+  let sobreSubtituloTaggat = document.querySelector("#sobreSubtituloTaggat");
+  let sobreSubtituloTagline = document.querySelector("#sobreSubtituloTagline");
 
-        TAGGATCaixa.addEventListener('click', function onclick(event){
-        console.log('Ouch!');
-        
-        if (TAGGATMostraCaixa.style.display === "block") { 
-            TAGGATMostraCaixa.style.display = "none"; 
-          } else { 
-            TAGGATMostraCaixa.style.display = "block"; 
-          }
+  let infoTaggat = document.querySelector("#sobreInfoTaggat");
+  let infoTagline = document.querySelector("#sobreInfoTagline");
 
-            TAGGATCaixa.classList.toggle("TAGGATCaixaPreto");
-            CaixaTAGGATTAGLINE.classList.toggle("TAGGATCaixaPreto");
-        });
-
-        TAGLINECaixa.addEventListener('click', function onclick2(event){
-            console.log('Ouch!2');
-            
-            if (TAGLINEMostraCaixa.style.display === "block") { 
-                TAGLINEMostraCaixa.style.display = "none"; 
-              } else { 
-                TAGLINEMostraCaixa.style.display = "block"; 
-              } 
-    
-              TAGLINECaixa.classList.toggle("TAGGATCaixaPreto");
-              CaixaTAGGATTAGLINE.classList.toggle("TAGGATCaixaPreto");
-            });*/
-
-    /*let CaixaTAGGATTAGLINE = document.querySelector('#CaixaTAGGATTAGLINE');
-    let TAGGATCaixa = document.querySelector('#TAGGATCaixa');
-    let TAGLINECaixa = document.querySelector('#TAGLINECaixa');
-    let TAGGATMostraCaixa = document.querySelector('#TAGGATMostraCaixa');
-    let TAGLINEMostraCaixa = document.querySelector('#TAGLINEMostraCaixa');
-
-    TAGGATCaixa.appendChild(TAGGATMostraCaixa);
-    TAGLINECaixa.appendChild(TAGLINEMostraCaixa);
-
-    function show(event) {
-      event.target.style.display = 'block';
-      console.log(event.target);
-    }
-
-    TAGGATCaixa.addEventListener('click', show, false);
-    */
-
-
-
-    //-----------------------------------------------------------------------------------------------------------------
-
-    /* tentativa 2
-    let TAGGATMostraCaixa = document.querySelector('#TAGGATMostraCaixa');
-    let TAGGATCaixa = document.querySelector('#TAGGATCaixa');
-    let TAGGATisOpen = false
-    
-    let TAGLINEMostraCaixa = document.querySelector('#TAGLINEMostraCaixa');
-    let TAGLINECaixa = document.querySelector('#TAGLINECaixa');
-    let TAGLINEisOpen = false
-    
-    let divConteudo = document.querySelector("#divConteudo");
-
-    TAGGATCaixa.appendChild(TAGGATMostraCaixa);
-    TAGLINECaixa.appendChild(TAGLINEMostraCaixa);
-
-    function handler(event){
-      var target = $(event.target);
-      if (target.is(TAGGATCaixa)){
-        target.children().toggle();
-      }
-    }
-
-    function handler2(event){
-      let target = $(event.target);
-      if (target.is(TAGGATCaixa)){
-        if(TAGGATisOpen == true){
-          closeTagat()
-        } else {
-          openTagat()
-          closeTagLine()
-        }
-      } else if (target.is(TAGLINECaixa)){
-        if(TAGLINEisOpen){
-          closeTagLine()
-        } else {
-          openTagLine()
-          closeTagat()
-        }
-      }
-
-    }
-
-    function openTagat(){
-      console.log('openTagat')
-        TAGGATCaixa.classList = ['TitulosPagSobreSelecionado']
-        divConteudo.classList = ['divConteudoAberto']
-        TAGGATisOpen = true;
-    }
-
-    function closeTagat(){
-      TAGGATCaixa.classList = ['TitulosPagSobre']
-      divConteudo.classList = ['divConteudoFechado']
-      TAGGATisOpen = false;
-  }
-
-    function openTagLine(){
-        TAGGATCaixa.classList = ['TitulosPagSobreSelecionado']
-        divConteudo.classList = ['divConteudoAberto']
-        TAGLINEisOpen = true;
-    }
-
-    function closeTagLine(){
-      TAGGATCaixa.classList = ['TitulosPagSobre']
-      
-      divConteudo.classList = ['divConteudoFechado']
-      TAGLINEisOpen = false;
-  }
-    
-    $(TAGGATCaixa).click(handler2).find(TAGGATCaixa).hide();
-  */
-
-
-
-
-  //--------------------------------------------------------------------------------------------------------------
-  // Tentativa 3
-
-  let TítuloTAGGAT = document.querySelector("#TítuloTAGGAT");
-  let TítuloTAGLINE = document.querySelector("#TítuloTAGLINE");
-
-  let TextoTAGGAT = document.querySelector("#TextoTAGGAT");
-  let TextoTAGLINE = document.querySelector("#TextoTAGLINE");
-
-  let ConteúdoTAGGATTAGLINE = document.querySelector("#ConteúdoTAGGATTAGLINE");
-
-
-  // TítuloTAGGAT.addEventListener('click', function showContentTAGGAT(){
-
-  //   console.log("hello");
-  //   if (ConteúdoTAGGATTAGLINE.style.display === "block") {
-  //     ConteúdoTAGGATTAGLINE.style.display = "none";
-  //     TextoTAGGAT.style.display = "none";
-  //     //TítuloTAGGAT.classList.remove('TAGGATCaixaPreto');
-  //   } else {
-  //     ConteúdoTAGGATTAGLINE.style.display = "block";
-  //     TextoTAGGAT.style.display = "block";
-  //     //TítuloTAGGAT.classList.add('TAGGATCaixaPreto');
-  // }
-  //   });
-
-  // TítuloTAGLINE.addEventListener('click', function showContentTAGLINE(){
-
-  //   console.log("helloagain");
-  //   if (ConteúdoTAGGATTAGLINE.style.display === "block") {
-  //     ConteúdoTAGGATTAGLINE.style.display = "none";
-  //     TextoTAGLINE.style.display = "none";
-  //     TítuloTAGLINE.classList.remove('TAGGATCaixaPreto');
-  //   } else {
-  //     ConteúdoTAGGATTAGLINE.style.display = "block";
-  //     TextoTAGLINE.style.display = "block";
-  //     TítuloTAGLINE.classList.add('TAGGATCaixaPreto');
-  // }});
-
-  // function onclickTAGGAT(event){
-  //   var target = $(event.target);
-  //   if (target.is(TítuloTAGGAT)){
-  //     console.log("click me");
-  //     TítuloTAGLINE.classList.remove('TAGGATCaixaPreto');
-  //     TítuloTAGGAT.classList.toggle('TAGGATCaixaPreto');
-  //     ConteúdoTAGGATTAGLINE.classList.toggle('conteudoTAGGATTAGLINEshow');
-  //     TextoTAGGAT.classList.toggle('textoTAGGATshow');
-  //   }
-  // }
-
-  
-  // function onclickTAGLINE(event){
-  //   var target = $(event.target);
-  //   if (target.is(TítuloTAGLINE)){
-  //     console.log("click me");
-  //     TítuloTAGGAT.classList.remove('TAGGATCaixaPreto');
-      
-  //   }
-  // }
-
-  // TítuloTAGGAT.addEventListener('click', onclickTAGGAT, false);
-  // TítuloTAGLINE.addEventListener('click', onclickTAGLINE, false);
-
-  //   };
-
-
-
-
-
-
-
-
-
+  let contentorInfo = document.querySelector("#sobreContentorInfo");
 
   // TENTATIVA DA GALIZA
 
-  TítuloTAGLINE.addEventListener('click', function showContentTAGLINE(){
+  sobreSubtituloTagline.addEventListener('click', function showContentTAGLINE(){
 
-    if (ConteúdoTAGGATTAGLINE.style.display === "block" && TítuloTAGLINE.classList.contains('TAGGATCaixaPreto')) {
-      ConteúdoTAGGATTAGLINE.style.display = "none";
-      TextoTAGLINE.style.display = "none";
-      TítuloTAGLINE.classList.remove('TAGGATCaixaPreto');
-    } else if (ConteúdoTAGGATTAGLINE.style.display === "block" && !TítuloTAGLINE.classList.contains('TAGGATCaixaPreto')) {
-      TextoTAGGAT.style.display = "none";
-      TítuloTAGGAT.classList.remove('TAGGATCaixaPreto');
-      TextoTAGLINE.style.display = "block";
-      TítuloTAGLINE.classList.add('TAGGATCaixaPreto');
+    if (contentorInfo.style.display === "block" && sobreSubtituloTagline.classList.contains('sobreVersaoPreto')) {
+      contentorInfo.style.display = "none";
+      infoTagline.style.display = "none";
+      sobreSubtituloTagline.classList.remove('sobreVersaoPreto');
+    } else if (contentorInfo.style.display === "block" && !sobreSubtituloTagline.classList.contains('sobreVersaoPreto')) {
+      infoTaggat.style.display = "none";
+      sobreSubtituloTaggat.classList.remove('sobreVersaoPreto');
+      infoTagline.style.display = "block";
+      sobreSubtituloTagline.classList.add('sobreVersaoPreto');
     } else {
-      ConteúdoTAGGATTAGLINE.style.display = "block";
-      TextoTAGLINE.style.display = "block";
-      TítuloTAGLINE.classList.add('TAGGATCaixaPreto');
+      contentorInfo.style.display = "block";
+      infoTagline.style.display = "block";
+      sobreSubtituloTagline.classList.add('sobreVersaoPreto');
     }
 });
 
-  TítuloTAGGAT.addEventListener('click', function showContentTAGGAT(){
+  sobreSubtituloTaggat.addEventListener('click', function showContentTAGGAT(){
 
-    if (ConteúdoTAGGATTAGLINE.style.display === "block" && TítuloTAGGAT.classList.contains('TAGGATCaixaPreto')) {
-      ConteúdoTAGGATTAGLINE.style.display = "none";
-      TextoTAGGAT.style.display = "none";
-      TítuloTAGGAT.classList.remove('TAGGATCaixaPreto');
-    } else if (ConteúdoTAGGATTAGLINE.style.display === "block" && !TítuloTAGGAT.classList.contains('TAGGATCaixaPreto')) {
-      TextoTAGLINE.style.display = "none";
-      TítuloTAGLINE.classList.remove('TAGGATCaixaPreto');
-      TextoTAGGAT.style.display = "block";
-      TítuloTAGGAT.classList.add('TAGGATCaixaPreto');
+    if (contentorInfo.style.display === "block" && sobreSubtituloTaggat.classList.contains('sobreVersaoPreto')) {
+      contentorInfo.style.display = "none";
+      infoTaggat.style.display = "none";
+      sobreSubtituloTaggat.classList.remove('sobreVersaoPreto');
+    } else if (contentorInfo.style.display === "block" && !sobreSubtituloTaggat.classList.contains('sobreVersaoPreto')) {
+      infoTagline.style.display = "none";
+      sobreSubtituloTagline.classList.remove('sobreVersaoPreto');
+      infoTaggat.style.display = "block";
+      sobreSubtituloTaggat.classList.add('sobreVersaoPreto');
     } else {
-      ConteúdoTAGGATTAGLINE.style.display = "block";
-      TextoTAGGAT.style.display = "block";
-      TítuloTAGGAT.classList.add('TAGGATCaixaPreto');
+      contentorInfo.style.display = "block";
+      infoTaggat.style.display = "block";
+      sobreSubtituloTaggat.classList.add('sobreVersaoPreto');
     }
 
   });
